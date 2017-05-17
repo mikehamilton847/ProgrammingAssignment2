0da1adfdde9ace14578e998dcbcc9d2909c6c37c
mikehamilton847 <mikehamilton847@gmail.com>
2017-05-17 15:12:43 -0500

    Functionally Complete
    Functionally Complete - Testing and documentation to follow

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,29 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(cacheMatrix) {
+  inverseCacheMatrix <<- NULL
+  set <- function(m) {
+    inverseCacheMatrix <<- NULL
+    cacheMatrix <<- m
+  }
+  get <- function() cacheMatrix
+  setInverse <- function(invertedMatrix) inverseCacheMatrix <<- invertedMatrix
+  getInverse <- function() inverseCacheMatrix
+  list(set=set, get=get,setInverse=setInverse,getInverse=getInverse)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(localMatrix, ...) {
         ## Return a matrix that is the inverse of 'x'
+  invertedMatrix <- localMatrix$getInverse()
+  if (!is.null(invertedMatrix)) {
+    message("Getting cached data")
+    return(invertedMatrix)
+  }
+  localData <-localMatrix$get()
+  #invertedMatrix <- solve(localData)
+  localMatrix$setInverse(solve(localData))
 }
