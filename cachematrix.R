924f67cfc4a2cbf66cdbf33f41282d384a0ebc25
mikehamilton847 <mikehamilton847@gmail.com>
2017-05-17 15:27:05 -0500

    Documentation Added to functions
    Added documentation and line to return the inverted matrix when it is
    not cached.

--- cachematrix.R
+++ cachematrix.R
@@ -1,31 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Function to keep track of current matrix, its inverse and to return information about the same
 
 makeCacheMatrix <- function(cacheMatrix) {
-  inverseCacheMatrix <<- NULL
+  #init inverse
+    inverseCacheMatrix <<- NULL
+    #set matrix
   set <- function(m) {
     inverseCacheMatrix <<- NULL
     cacheMatrix <<- m
   }
+  #return matrix
   get <- function() cacheMatrix
+  #set inverse matrix value
   setInverse <- function(invertedMatrix) inverseCacheMatrix <<- invertedMatrix
+  #get cached inverse
   getInverse <- function() inverseCacheMatrix
   list(set=set, get=get,setInverse=setInverse,getInverse=getInverse)
 }
 
 
-## Write a short comment describing this function
+## Function to print an inverted matrix uses makeCacheMatrix
 
 cacheSolve <- function(localMatrix, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  ## Return a matrix that is the inverse of 'x'
+  
+  #Get cached matrix
   invertedMatrix <- localMatrix$getInverse()
   if (!is.null(invertedMatrix)) {
+    #if there is a cached matrix, message and return the inverse
     message("Getting cached data")
     return(invertedMatrix)
   }
+  #get current matrix
   localData <-localMatrix$get()
-  #invertedMatrix <- solve(localData)
-  localMatrix$setInverse(solve(localData))
+  #invert the matrix and save it to the cache
+  localInvertedMatrix = solve(localData)
+  localMatrix$setInverse(localInvertedMatrix)
+  localInvertedMatrix
 }
